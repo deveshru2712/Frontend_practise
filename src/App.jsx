df0f5ac2cde8ef5d4bf6e24c6a0d7da06668c9b3
Devesh Chandra <yashdev2712@gmail.com>
2025-02-24 13:31:56 +0530

    added banner section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,21 @@ import Hero from "./components/Hero/Hero";
 import Category from "./components/Category/Category";
 import Category2 from "./components/Category/Category2";
 import Services from "./components/Services/Services";
+import Banner from "./components/Banner/Banner";
+
+import headphone from "./assets/hero/headphone.png";
+
+const BannerData = {
+  discount: "30% Off",
+  title: "Fine Smile",
+  data: "10 Jan to 28 Jan",
+  image: headphone,
+  title2: "Air Solo Bass",
+  title3: "Winter Sale",
+  title4:
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quibusdam hic ipsum dolor nulla id rerum corrupti sequi minus nostrum quae?",
+  bgColor: "#f42c37",
+};
 
 const App = () => {
   return (
@@ -13,6 +28,7 @@ const App = () => {
       <Category />
       <Category2 />
       <Services />
+      <Banner data={BannerData} />
     </div>
   );
 };
